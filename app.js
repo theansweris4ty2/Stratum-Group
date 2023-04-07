44879e12d9c271dcbf389f826943be476c6ee506
Marty <mdjd2001@gmail.com>
2023-04-07 11:45:41 -0700

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+app.use(express.static("./public"))
+app.use(express.json())
 const port = process.env.PORT || 3000
 
-const start = async () => {
-  app.listen(port)
-  console.log(`Server listening on port ${port}`)
-}
+
+  const start = ()=> {app.listen(port, ()=>
+  console.log(`Server listening on port ${port}`))}
+
 start()
